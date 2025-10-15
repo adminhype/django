319ec3b58668c02905d322a0e795230943c86d85
Adem <ado.designs@outlook.de>
2025-10-15 23:37:17 +0200

    view urlpattern dummy_data and JsonResponse

--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
+import json
+
+from .dummy_data import gadgets
 # Create your views here.
+
+
+def start_page_view(request):
+    return HttpResponse("me: Welcome too Views")
+
+
+def single_gadget_view(request):
+    return JsonResponse(gadgets[0])
